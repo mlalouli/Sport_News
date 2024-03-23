9ebf8100754a872b5d6506ac85d672f59008e864
mlalouli <meryemlalouli@gmail.com>
2024-03-23 03:58:06 +0100

    Add centent the news image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div >
   <v-card>
+
+ <!-- Nav-bar -->
+
     <v-layout>
       <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
 
@@ -21,7 +24,8 @@
                     src="https://www.livescore.com/ls-web-assets/svgs/common/livescore-logo-b3b211143dccd9e22d164701d32a390f.svg"
           >
         </v-toolbar-title>
-  <div class="d-flex justify-space-around">
+<!-- top-bar -->
+     <div class="d-flex justify-space-around">
     <v-btn>
       <img 
         alt="Scores" 
@@ -105,7 +109,7 @@
         
   <v-card-text>
 
-    <div style="background-color: red; width: 50%;margin-left: 25%;  ">      
+    <div style="background-color: #363535; width: 100%;margin-left: 0%;  ">      
   <v-sheet class="mx-auto" max-width="600">
     <v-slide-group show-arrows>
       <v-slide-group-item
@@ -126,28 +130,210 @@
       </v-slide-group-item>
     </v-slide-group>
   </v-sheet>
+
+   
+    <div >
   
+        <div class="Mi">
+          <div class="kl ll" id="news-news-list-item-2024032222145404834">
+              <a id="article-image-link-2024032222145404834" >
+                <div class="hl" id="article-image-wrapper-2024032222145404834">
+                  <img  
+                    data-src="https://uk1.sportal365images.com/process/smp-image-api/livescore.com/22032024/14f56de9-b7bf-4437-b3cc-fd435f82e7e9.jpg?operations=fit(260:)&amp;w=260&amp;quality=100" loading="lazy" width="260" height="160" decoding="async" data-nimg="1" src="https://uk1.sportal365images.com/process/smp-image-api/livescore.com/22032024/14f56de9-b7bf-4437-b3cc-fd435f82e7e9.jpg?operations=fit(260:)&amp;w=260&amp;quality=100" style="color: transparent;">
+                </div>
+             </a>
+          <div class="ol" id="article-item-content-2024032222145404834">
+              <a class="ql" id="article-item-title-link-2024032222145404834" >
+                <h2 class="ml" id="article-item-content-title-2024032222145404834">
+                  Robertson admits Scots must improve at Euros after Netherlands collapse
+                </h2>
+              </a>
+                <div class="pl" id="article-item-footer-2024032222145404834">
+                    <div id="article-list-tags-2024032222145404834" class="Lq">
+                      <a id="tag-link-Scotland" class="Mq" >
+                        <span id="tag-label-Scotland">
+                          <span>Scotland</span>
+                        </span>
+                      </a>
+                      <a id="tag-link-Andrew Robertson" class="Mq">
+                        <span id="tag-label-Andrew Robertson">
+                        <span>Andrew Robertson</span>
+                        </span>
+                      </a>
+                      <a id="tag-link-Netherlands" class="Mq" ><span id="tag-label-Netherlands">
+                        <span>Netherlands</span>
+                        </span>
+                      </a>
+                    </div>
+                    <a class="rl" id="article-item-footer-link-2024032222145404834" >
+                      <div class="tl" id="article-item-date-2024032222145404834">1 hour ago</div>
+                    </a>
+                </div>
+            </div>
+          </div></div>
+        <div class="Mi">
+          <div class="kl ll" id="news-news-list-item-2024032222145404834">
+            <a id="article-image-link-2024032222145404834" >
+              <div class="hl" id="article-image-wrapper-2024032222145404834">
+                <img 
+                  alt="Steve Clarke’s Scotland were hammered in Amsterdam (Joris Verwijst/PA)" id="article-item-image-2024032221513452165" 
+                  data-src="https://image.assets.pressassociation.io/v2/image/production/65c751f471aba4367cd8fe40f3186c77Y29udGVudHNlYXJjaGFwaSwxNzExMjMwMTY4/2.75691725.jpg?rect=566,150,3202,1801&amp;ext=.jpg&amp;operations=fit(260:)&amp;w=260&amp;quality=100" loading="lazy" width="260" height="160" decoding="async" data-nimg="1" src="https://image.assets.pressassociation.io/v2/image/production/65c751f471aba4367cd8fe40f3186c77Y29udGVudHNlYXJjaGFwaSwxNzExMjMwMTY4/2.75691725.jpg?rect=566,150,3202,1801&amp;ext=.jpg&amp;operations=fit(260:)&amp;w=260&amp;quality=100" 
+                  style="color: transparent;">
+              </div>
+            </a>
+          <div class="ol" id="article-item-content-2024032222145404834">
+              <a class="ql" id="article-item-title-link-2024032222145404834" >
+                <h2 class="ml" id="article-item-content-title-2024032222145404834">
+                  Netherlands 4-0 Scotland: Tartan Army crumble in Amsterdam
+                </h2>
+              </a>
+                <div class="pl" id="article-item-footer-2024032222145404834">
+                    <div id="article-list-tags-2024032222145404834" class="Lq">
+                      <a id="tag-link-Scotland" class="Mq" >
+                        <span id="tag-label-Scotland">
+                          <span>Scotland</span>
+                        </span>
+                      </a>
+                    <a id="tag-link-Andrew Robertson" class="Mq">
+                      <span id="tag-label-Andrew Robertson">
+                        <span>Andrew Robertson</span>
+                      </span>
+                    </a>
+                    <a id="tag-link-Netherlands" class="Mq" ><span id="tag-label-Netherlands"><span>Netherlands</span></span></a></div><a class="rl" id="article-item-footer-link-2024032222145404834" ><div class="tl" id="article-item-date-2024032222145404834">1 hour ago</div>
+                    </a>
+                  </div>
+            </div>
+            </div></div>
+        <div class="Mi">
+          <div class="kl ll" id="news-news-list-item-2024032222145404834">
+            <a id="article-image-link-2024032222145404834" >
+              <div class="hl" id="article-image-wrapper-2024032222145404834">
+                <img alt="Northern Ireland’s Jamie Reid celebrates his goal (Andreea Alexandru/AP)." id="article-item-image-2024032221513665000" data-src="https://content.assets.pressassociation.io/AP/2024/03/22/c49f3c03d1ad4d968a8a2f57a206f920.jpg?rect=0,137,5044,2837&amp;ext=.jpg&amp;operations=fit(260:)&amp;w=260&amp;quality=100" loading="lazy" width="260" height="160" decoding="async" data-nimg="1" src="https://content.assets.pressassociation.io/AP/2024/03/22/c49f3c03d1ad4d968a8a2f57a206f920.jpg?rect=0,137,5044,2837&amp;ext=.jpg&amp;operations=fit(260:)&amp;w=260&amp;quality=100" style="color: transparent;"
+                />
+              </div>
+            </a>
+          <div class="ol" id="article-item-content-2024032222145404834">
+              <a class="ql" id="article-item-title-link-2024032222145404834" >
+                <h2 class="ml" id="article-item-content-title-2024032222145404834">
+                  Romania 1-1 Northern Ireland: Reid’s debut goal earns encouraging away draw
+                </h2>
+              </a>
+                <div class="pl" id="article-item-footer-2024032222145404834">
+                    <div id="article-list-tags-2024032222145404834" class="Lq">
+                      <a id="tag-link-Scotland" class="Mq" >
+                        <span id="tag-label-Scotland">
+                          <span>Scotland</span>
+                        </span>
+                      </a>
+                    <a id="tag-link-Andrew Robertson" class="Mq">
+                      <span id="tag-label-Andrew Robertson">
+                        <span>Andrew Robertson</span>
+                      </span>
+                    </a>
+                    <a id="tag-link-Netherlands" class="Mq" ><span id="tag-label-Netherlands"><span>Netherlands</span></span></a></div><a class="rl" id="article-item-footer-link-2024032222145404834" ><div class="tl" id="article-item-date-2024032222145404834">1 hour ago</div>
+                    </a>
+                  </div>
+            </div>
+            </div></div>
+        <div class="Mi">
+          <div class="kl ll" id="news-news-list-item-2024032222145404834">
+            <a id="article-image-link-2024032222145404834" >
+              <div class="hl" id="article-image-wrapper-2024032222145404834">
+                <img alt="Paul Warne's Derby have won their last four games, keeping three clean sheets, to take a grip of second place" id="article-item-image-2024032212390729300" data-src="https://uk1.sportal365images.com/process/smp-image-api/livescore.com/22032024/9b5b8e91-f23e-4c07-bb07-1c73cf60ff31.jpg?operations=fit(260:)&amp;w=260&amp;quality=100" loading="lazy" width="260" height="160" decoding="async" data-nimg="1" src="https://uk1.sportal365images.com/process/smp-image-api/livescore.com/22032024/9b5b8e91-f23e-4c07-bb07-1c73cf60ff31.jpg?operations=fit(260:)&amp;w=260&amp;quality=100" 
+                  style="color: transparent;"
+                />
+              </div>
+            </a>
+          <div class="ol" id="article-item-content-2024032222145404834">
+              <a class="ql" id="article-item-title-link-2024032222145404834" >
+                <h2 class="ml" id="article-item-content-title-2024032222145404834">
+                  Saturday EFL predictions: Four teams to follow in the EFL
+                </h2>
+              </a>
+                <div class="pl" id="article-item-footer-2024032222145404834">
+                    <div id="article-list-tags-2024032222145404834" class="Lq">
+                      <a id="tag-link-Scotland" class="Mq" >
+                        <span id="tag-label-Scotland">
+                          <span>Scotland</span>
+                        </span>
+                      </a>
+                    <a id="tag-link-Andrew Robertson" class="Mq">
+                      <span id="tag-label-Andrew Robertson">
+                        <span>Andrew Robertson</span>
+                      </span>
+                    </a>
+                    <a id="tag-link-Netherlands" class="Mq" ><span id="tag-label-Netherlands"><span>Netherlands</span></span></a></div><a class="rl" id="article-item-footer-link-2024032222145404834" ><div class="tl" id="article-item-date-2024032222145404834">1 hour ago</div>
+                    </a>
+                  </div>
+            </div></div></div>
+        <div class="Mi">
+          <div class="kl ll" id="news-news-list-item-2024032222145404834">
+            <a id="article-image-link-2024032222145404834" >
+              <div class="hl" id="article-image-wrapper-2024032222145404834">
+                <img alt="Neco Williams (second left) celebrates after scoring Wales’ second goal in their 4-1 Euro 2024 play-off victory over Finland (Nick Potts/PA)" id="article-item-image-2024032121553630141" data-src="https://image.assets.pressassociation.io/v2/image/production/ab63348f615b573478e750755dc1d244Y29udGVudHNlYXJjaGFwaSwxNzExMTQzMzEw/2.75683709.jpg?rect=0,238,5432,3055&amp;ext=.jpg&amp;operations=fit(260:)&amp;w=260&amp;quality=100" loading="lazy" width="260" height="160" decoding="async" data-nimg="1" src="https://image.assets.pressassociation.io/v2/image/production/ab63348f615b573478e750755dc1d244Y29udGVudHNlYXJjaGFwaSwxNzExMTQzMzEw/2.75683709.jpg?rect=0,238,5432,3055&amp;ext=.jpg&amp;operations=fit(260:)&amp;w=260&amp;quality=100" style="color: transparent;"
+                />
+              </div>
+            </a>
+          <div class="ol" id="article-item-content-2024032222145404834">
+              <a class="ql" id="article-item-title-link-2024032222145404834" >
+                <h2 class="ml" id="article-item-content-title-2024032222145404834">
+                  Wales 4-1 Finland: Dragons set up Poland showdown for Euros spot
+                </h2>
+              </a>
+                <div class="pl" id="article-item-footer-2024032222145404834">
+                    <div id="article-list-tags-2024032222145404834" class="Lq">
+                      <a id="tag-link-Scotland" class="Mq" >
+                        <span id="tag-label-Scotland">
+                          <span>Scotland</span>
+                        </span>
+                      </a>
+                    <a id="tag-link-Andrew Robertson" class="Mq">
+                      <span id="tag-label-Andrew Robertson">
+                        <span>Andrew Robertson</span>
+                      </span>
+                    </a>
+                    <a id="tag-link-Netherlands" class="Mq" ><span id="tag-label-Netherlands"><span>Netherlands</span></span></a></div><a class="rl" id="article-item-footer-link-2024032222145404834" ><div class="tl" id="article-item-date-2024032222145404834">1 hour ago</div>
+                    </a>
+                  </div>
+            </div>
+            </div></div>
+        <div class="Mi">
+                  <div class="kl ll" id="news-news-list-item-2024032222145404834">
+                    <a id="article-image-link-2024032222145404834" >
+                      <div class="hl" id="article-image-wrapper-2024032222145404834">
+                        <img alt="Gareth Southgate's England are on a 10-match unbeaten run" id="article-item-image-2024032115272722222" data-src="https://uk1.sportal365images.com/process/smp-image-api/livescore.com/21032024/963427c5-4e7e-4de1-9f5b-c25948ee5d6b.jpg?operations=fit(260:)&amp;w=260&amp;quality=100" loading="lazy" width="260" height="160" decoding="async" data-nimg="1" src="https://uk1.sportal365images.com/process/smp-image-api/livescore.com/21032024/963427c5-4e7e-4de1-9f5b-c25948ee5d6b.jpg?operations=fit(260:)&amp;w=260&amp;quality=100" style="color: transparent;"
+                        />
+                      </div>
+                    </a>
+                  <div class="ol" id="article-item-content-2024032222145404834">
+                      <a class="ql" id="article-item-title-link-2024032222145404834" >
+                        <h2 class="ml" id="article-item-content-title-2024032222145404834">
+                          England vs Brazil predictions: Glamour friendly to end all-square
+                        </h2>
+                      </a>
+                        <div class="pl" id="article-item-footer-2024032222145404834">
+                            <div id="article-list-tags-2024032222145404834" class="Lq">
+                              <a id="tag-link-Scotland" class="Mq" >
+                                <span id="tag-label-Scotland">
+                                  <span>Scotland</span>
+                                </span>
+                              </a>
+                            <a id="tag-link-Andrew Robertson" class="Mq">
+                              <span id="tag-label-Andrew Robertson">
+                                <span>Andrew Robertson</span>
+                              </span>
+                            </a>
+                            <a id="tag-link-Netherlands" class="Mq" ><span id="tag-label-Netherlands"><span>Netherlands</span></span></a></div><a class="rl" id="article-item-footer-link-2024032222145404834" ><div class="tl" id="article-item-date-2024032222145404834">1 hour ago</div>
+                            </a>
+                          </div>
+                    </div></div></div>
 
 
-    <v-carousel hide-delimiters  style="width: 80%;height: 30%; margin-left: 9%;">
-      <v-carousel-item
-      v-for="(item,i) in item"
-      :key="i"
-      :src="item.src"
-      cover
-      width="100%"
-      
-      > <div class="d-flex flex-column fill-height justify-center align-center text-white">
-        
-        <h3 class="Gt" data-testid="article-slide_title"  >Lescott: Who Id pick to help England win Euro 2024</h3>
-      </div></v-carousel-item>
-    </v-carousel>
+    </div>
   </div>
   
 </v-card-text>
-<div style="height: 100%;width: 50%;background-color: #ff6b00; margin-left: 25%;">
-  
-</div>
+
 </v-main>
 </v-layout>
 <div style="height: 50%;width: 100%;background-color: red;"></div>
@@ -220,11 +406,21 @@
             src: 'https://uk1.sportal365images.com/process/smp-image-api/livescore.com/18032024/7cd41289-670d-4946-a198-4692b729a20b.jpg?operations=fit(620:)&w=620&quality=100',
           },
         ],
-        news: [
-          'Lescott: Who Id pick to help England win Euro 2024',
-          'fgijbnijfg',
 
-          'fkvmofsmvo',
+        
+        news: [
+          {
+            comment: 'Lescott: Who Id pick to help England win Euro 2024'
+          },
+          {
+            comment: 'fgijbnijfg',
+          },
+          {
+            comment: 'fkvmofsmvo',
+          }
+          
+
+          
         ],
     }),
     
@@ -236,3 +432,4 @@
     },
   }
 </script>
+
